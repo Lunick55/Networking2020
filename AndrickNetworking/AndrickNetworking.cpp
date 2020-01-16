3deadb12f51e75c9fcf764152589998ed02ff0d8
Andrew Rimpici <AndrewR608@gmail.com>
2020-01-16 14:01:05 -0500

    Made input a bit more robust. Now properly able to choose either client or server.

--- AndrickNetworking/AndrickNetworking.cpp
+++ AndrickNetworking/AndrickNetworking.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-void getInput(const char* outputText, std::string input)
+void getInput(const char* outputText, std::string& input)
 {
 	printf(outputText);
 	std::getline(std::cin, input);
@@ -11,37 +11,51 @@ void getInput(const char* outputText, std::string input)
 
 int main(void)
 {
-	const int MAX_COUNT = 512;
-	unsigned int maxClients = 0;
-	unsigned short serverPort = 7777;
+	unsigned int maxClients = 10;
+	unsigned short serverPort = 600;
 
-	char str[MAX_COUNT];
 	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
 	bool isServer;
 
-	getInput("(C) or (S)erver?\n", str);
+	//Find out if this is a client or server instance.
+	std::string clientServerInput;
+	getInput("(C) or (S)erver?\n", clientServerInput);
 
-	//Max Clients is only initialized if server is being used.
-	std::string maxClientInput;
-	getInput("Max Clients: ", maxClientInput);
+	//Default to client if invalid input.
+	const char csResult = clientServerInput.empty() ? 'c' : clientServerInput[0];
 
 	//Server port is always initialized for server and client.
 	std::string serverPortInput;
 	getInput("Server Port: ", serverPortInput);
 
-	fgets(str, MAX_COUNT, stdin);
-	if ((str[0] == 'c') || (str[0] == 'C'))
+	//Client
+	if (std::tolower(csResult) == 'c')
 	{
 		RakNet::SocketDescriptor sd;
 		peer->Startup(1, &sd, 1);
 		isServer = false;
 	}
-	else {
+	//Server
+	else 
+	{
+		//Max Clients is only initialized if server is being used.
+		std::string maxClientInput;
+		getInput("Max Clients: ", maxClientInput);
+
+		try
+		{
+			maxClients = std::stoi(maxClientInput);
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << "Could not convert Max Client input into a number: " << e.what() << std::endl;
+			return -1;
+		}
+
 		RakNet::SocketDescriptor sd(serverPort, 0);
 		peer->Startup(maxClients, &sd, 1);
 		isServer = true;
 	}
-
 
 	// TODO - Add code body here
 
