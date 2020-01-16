536d173996494e0b0852030ca292d96bb43cdd13
Andrew Rimpici <AndrewR608@gmail.com>
2020-01-16 10:29:14 -0500

    Start lab01

--- AndrickNetworking/AndrickNetworking.cpp
+++ AndrickNetworking/AndrickNetworking.cpp
@@ -1,20 +1,34 @@
-// AndrickNetworking.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <RakNet/RakPeerInterface.h>
 
-#include <iostream>
-
-int main()
+int main(void)
 {
-    std::cout << "Hello World!\n";
+	const int MAX_COUNT = 512;
+	unsigned int maxClients;
+	unsigned short serverPort;
+
+	char str[MAX_COUNT];
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	printf("(C) or (S)erver?\n");
+	fgets(str, MAX_COUNT, stdin);
+	if ((str[0] == 'c') || (str[0] == 'C'))
+	{
+		RakNet::SocketDescriptor sd;
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+	}
+	else {
+		RakNet::SocketDescriptor sd(serverPort, 0);
+		peer->Startup(maxClients, &sd, 1);
+		isServer = true;
+	}
+
+
+	// TODO - Add code body here
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
