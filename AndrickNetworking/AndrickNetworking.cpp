b203d65e1e8b8629ca3519e3666793e6cc59e771
Andrew Rimpici <AndrewR608@gmail.com>
2020-01-16 10:52:35 -0500

    Implementing lab01 input server client

--- AndrickNetworking/AndrickNetworking.cpp
+++ AndrickNetworking/AndrickNetworking.cpp
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <RakNet/RakPeerInterface.h>
+#include <iostream>
+#include <string>
+
+void getInput(const char* outputText, std::string input)
+{
+	printf(outputText);
+	std::getline(std::cin, input);
+}
 
 int main(void)
 {
 	const int MAX_COUNT = 512;
-	unsigned int maxClients;
-	unsigned short serverPort;
+	unsigned int maxClients = 0;
+	unsigned short serverPort = 7777;
 
 	char str[MAX_COUNT];
 	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
 	bool isServer;
 
-	printf("(C) or (S)erver?\n");
+	getInput("(C) or (S)erver?\n", str);
+
+	//Max Clients is only initialized if server is being used.
+	std::string maxClientInput;
+	getInput("Max Clients: ", maxClientInput);
+
+	//Server port is always initialized for server and client.
+	std::string serverPortInput;
+	getInput("Server Port: ", serverPortInput);
+
 	fgets(str, MAX_COUNT, stdin);
 	if ((str[0] == 'c') || (str[0] == 'C'))
 	{
