f4337d7fecfdfa543ce21980984c875f16ea6402
Karim <karimelzanaty98@gmail.com>
2020-01-18 18:25:39 -0500

    Added part 2 of tutorial.

--- AndrickNetworking/AndrickNetworking.cpp
+++ AndrickNetworking/AndrickNetworking.cpp
@@ -2,7 +2,9 @@
 #include <RakNet/RakPeerInterface.h>
 #include <iostream>
 #include <string>
+#include "RakNet/MessageIdentifiers.h"
 
+//is this really necessary?
 void getInput(const char* outputText, std::string& input)
 {
 	printf(outputText);
@@ -16,6 +18,7 @@ int main(void)
 
 	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
 	bool isServer;
+	RakNet::Packet* packet;
 
 	//Find out if this is a client or server instance.
 	std::string clientServerInput;
@@ -55,6 +58,72 @@ int main(void)
 		RakNet::SocketDescriptor sd(serverPort, 0);
 		peer->Startup(maxClients, &sd, 1);
 		isServer = true;
+	}
+
+	if (isServer)
+	{
+		printf("Starting the server.\n");
+		// We need to let the server accept incoming connections from the clients
+		peer->SetMaximumIncomingConnections(maxClients);
+	}
+	else {
+		std::cout << ("Enter server IP or hit enter for 127.0.0.1\n");
+		std::string serverIPInput;
+		getInput("Server IP: ", serverIPInput);
+		if (!serverIPInput.empty()) {
+			serverIPInput = "127.0.0.1";
+		}
+		printf("Starting the client.\n");
+
+		peer->Connect(serverIPInput.c_str(), serverPort, 0, 0);
+
+	}
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
 	}
 
 	// TODO - Add code body here
