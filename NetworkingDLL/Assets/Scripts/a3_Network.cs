f81485708b11897805c1965b63df860c87f8eba3
Andrew Rimpici <AndrewR608@gmail.com>
2020-02-24 10:33:19 -0500

    In class, using animal as DLL to render in Unity, but I couldn't get the plugin to load :(

---
+++ NetworkingDLL/Assets/Scripts/a3_Network.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class a3_Network
+{
+    [DllImport("animal3D-Networking_x86")]
+    static extern public int foo(int bar);
+
+
+    [DllImport("animal3D-Networking_x86")]
+    static extern public void sendVector(float x, float y);
+
+    [DllImport("animal3D-Networking_x86")]
+    static extern public void receiveVector(out float x, out float y);//In C = void receiveVector(float* x, float* y)
+}
